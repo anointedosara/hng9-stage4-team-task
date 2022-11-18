d3fcd0fa778c64df70bf1ac47ffb0fd1108b3113
anointed osara <anointedosara@gmail.com>
2022-11-19 01:04:01 +0530

    feat: Adding MyApplication and Making the webpage responsive

--- src/components/MyApplications.js
+++ src/components/MyApplications.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import myApplicationData from '../data'
 
 function MyApplications() {
+  const data = myApplicationData
   return (
-    <div>
-      leieouir
+    <div className='container'>
+      <div className='myapplication'>
+        <div className="myapplication-head">
+        <p>My Applications</p>
+        <p>See all</p>
+        </div>
+        <div className='applications'>
+        {
+          data?.map((item, i) => <div className='application' key={i}>
+            <div className='application-title'>
+              <div className="img-div"><img src={item?.image} alt="" /></div>
+              <div>
+                <h1>{item?.title}</h1>
+                <p>{item?.description}</p>
+              </div>
+            </div>
+
+              <p>{item?.type}</p>
+
+              <p>{item?.time}</p>
+          </div>)
+        }
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,39 @@
+const myApplicationData = [
+    {
+        image: './images/figma-logo.svg',
+        title: 'Front-end Dev',
+        description: 'Figma',
+        type: 'Remote',
+        time: '10 hours ago'
+    },
+    {
+        image: './images/instagram-logo.svg',
+        title: 'UI/UX Designer',
+        description: 'Instagram',
+        type: 'Full-Time',
+        time: '19 hours ago'
+    },
+    {
+        image: './images/web.svg',
+        title: 'Web Designer',
+        description: 'Globex Corporation ',
+        type: 'Remote',
+        time: '2 days ago'
+    },
+    {
+        image: './images/pinterest-logo.svg',
+        title: 'Visual Designer',
+        description: 'Pinterest',
+        type: 'Full-Time',
+        time: '7 days ago'
+    },
+    {
+        image: './images/google-logo.svg',
+        title: 'Product Designer',
+        description: 'Google',
+        type: 'Full-Time',
+        time: '2 weeks ago'
+    }
+]
+
+export default myApplicationData
