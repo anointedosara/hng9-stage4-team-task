87faef66625e50316bf5031aa0f01bef1455b21a
anointed osara <anointedosara@gmail.com>
2022-11-26 14:24:28 +0530

    fix:fixing dashboard index code

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,8 +1,6 @@
-import React from 'react'
 import { useParams } from 'react-router-dom'
 
 function Overview() {
-  const params = useParams()
   const data = [
     {
       number: 72,
@@ -23,7 +21,7 @@ function Overview() {
       <div className='overview-grid'>
         {
           data?.map((item, i) => <div key={i}>
-            <h1>{params?.newuser === 'newuser' ? '0' : item?.number}</h1>
+            <h1>{item?.number}</h1>
             <p>{item?.text}</p>
             </div>)
         }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
 import Body from '../components/Body'
-import NewUser from '../components/NewUser'
 import SideNav from '../components/SideNav'
 
 function Dashboard() {
   return (
     <div className='dashboard'>
       <SideNav />
-      <Route path='/' exact component={Body} />
-      <Route path='/:newuser' component={NewUser} />
+      <Body />
     </div>
   )
 }
